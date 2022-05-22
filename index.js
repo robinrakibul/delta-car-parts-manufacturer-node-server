e4d3caa301c704793f39997ee1a69635b08348f0
robinrakibul <robinrakibulhassan@gmail.com>
2022-05-22 10:18:02 +0600

    connected to mongodb server, added data to database carParts, used dotenv to hide username password

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const port = process.env.PORT || 5000;
+const app = express();
+const { query } = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 // middleware
 app.use(cors());
@@ -10,6 +13,32 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('My server is running');
 })
+
+// mongodb connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.qj1eq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        // collection in mongodb
+        const itemsCollection = client.db('carParts').collection('items');
+
+        // items API
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log('Listening to port', port);
