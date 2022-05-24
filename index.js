110efc19e5de700b5a12e355dde16e9060534cd2
robinrakibul <robinrakibulhassan@gmail.com>
2022-05-24 13:04:10 +0600

    admin email get for user role check

--- index.js
+++ index.js
@@ -105,6 +105,14 @@ async function run() {
             res.send(result);
         })
 
+        // admin email check 
+        app.get('/admin/:email',async(req,res)=>{
+            const email = req.params.email;
+            const user = await userCollection.findOne({email: email});
+            const isAdmin = user.role === 'admin';
+            res.send({admin: isAdmin})
+        })
+
         // searchId Items
         app.get('/items/:id', async (req, res) => {
             const id = req.params.id;
